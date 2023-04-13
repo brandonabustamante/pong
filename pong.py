510a2f15eb116772d395712b4f02420115f46636
Brandon A Bustamante <brandonabus1995@gmail.com>
2023-04-13 11:46:54 -0500

    Paddle class added and started draw methods

--- pong.py
+++ pong.py
@@ -1,3 +1,4 @@
+################################################################################
 import pygame
 
 pygame.init()
@@ -5,17 +6,51 @@ pygame.init()
 #   CONSTANTS
 WIDTH = 700
 HEIGHT = 500
-# tuple with two parameters
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 60
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+# input is tuple with two parameters
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+PADDLE_WIDTH = 20
+PADDLE_HEIGHT = 100
+
 # Window title
 pygame.display.set_caption("Pong")
 
+class Paddle:
+    COLOR = WHITE
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+def draw(win, paddles):
+    win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    pygame.display.update()
 
 def main():
     run = True
+    clock = pygame.time.Clock()
+
+    # setting left paddle to ceter
+    left_paddle = Paddle(10, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    # setting right paddle to center 
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
 
     # main game loop
     while run:
+        clock.tick(FPS)
+        draw(WIN, [left_paddle, right_paddle])
         # getting all events IE mouse clicks, keyboard inputs
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
